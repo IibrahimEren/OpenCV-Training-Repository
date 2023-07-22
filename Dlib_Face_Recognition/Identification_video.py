2c25f529695dec49755428be2528cd0c2d49d1a6
İbrahim <87008174+IibrahimEren@users.noreply.github.com>
2023-07-22 14:03:55 +0300

    Delete Identification_video.py

--- Dlib_Face_Recognition/Identification_video.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import face_recognition
-
-# Load the image of Jensen for training.
-path = 'media/jensen.png'
-jensenImage = cv2.imread(path)
-jensenImage_encoding = face_recognition.face_encodings(jensenImage)[0]
